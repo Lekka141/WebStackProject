3465a75afab5aa6df523ce18bdfd13baee4a253b
ManhicaThomas <thomasmanchica@gmail.com>
2024-10-10 04:22:09 -0700

    update

--- Backend/src/routes/fileRoutes.js
+++ Backend/src/routes/fileRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const { uploadFile, getUserFiles, deleteFile } = require('../controllers/fileController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+app.post('/upload', fileController.uploadFile);
+app.get('/files/:fileId', fileController.getFileData);
+app.delete('/files/:fileId', fileController.deleteFile);
+
 // Protected file routes
 router.post('/upload', authMiddleware, uploadFile);
 router.get('/files', authMiddleware, getUserFiles);
